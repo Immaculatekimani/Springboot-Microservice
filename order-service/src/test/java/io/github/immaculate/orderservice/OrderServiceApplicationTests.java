5873d872fe04f0ecc7ae7cf58951e6ba77e833d0
Immaculatekimani <immaculatewanjira21@gmail.com>
2024-03-19 16:36:25 +0300

    Microservice setup
    Added microservices

---
+++ order-service/src/test/java/io/github/immaculate/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.immaculate.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
